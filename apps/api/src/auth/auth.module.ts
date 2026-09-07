5aed19a3c9cd9dc9a855a251daa3d930c2e15c1f
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 20:23:38 +0530

    Register auth guard

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { AuthGuard } from "./auth.guard";
 import { TokenService } from "./token.service";
 import { PrismaService } from "../prisma.service";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, TokenService, PrismaService],
+  providers: [AuthService, AuthGuard, TokenService, PrismaService],
+  exports: [AuthGuard, TokenService],
 })
 export class AuthModule {}
