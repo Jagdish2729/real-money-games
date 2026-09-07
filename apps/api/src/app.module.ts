4bad6dc7f84465d074732d7e59966e056897bfd3
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 20:00:36 +0530

    Register authentication module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { PrismaService } from "./prisma.service";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [PrismaService],
   exports: [PrismaService],
