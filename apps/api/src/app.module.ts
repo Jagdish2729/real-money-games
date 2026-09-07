2de2859a7bcf0049f6e63f0c16c9fd222edac88a
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 20:53:30 +0530

    feat(api): register admin module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from "./prisma.service";
 import { AuthModule } from "./auth/auth.module";
 import { WalletModule } from "./wallet/wallet.module";
 import { DepositModule } from "./deposit/deposit.module";
+import { AdminModule } from "./admin/admin.module";
 
 @Module({
-  imports: [AuthModule, WalletModule, DepositModule],
+  imports: [AuthModule, WalletModule, DepositModule, AdminModule],
   controllers: [AppController],
   providers: [PrismaService],
   exports: [PrismaService],
