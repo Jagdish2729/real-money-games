c4f4bd8d94ff437bea4938a6dde16633d8eb0a06
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 19:54:05 +0530

    Register Prisma service in API module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { PrismaService } from "./prisma.service";
 
 @Module({
   controllers: [AppController],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
