9c0c0b0491a65f780be8fe53501585e1fccaea28
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 20:44:15 +0530

    feat(api): register deposit module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { PrismaService } from "./prisma.service";
 import { AuthModule } from "./auth/auth.module";
 import { WalletModule } from "./wallet/wallet.module";
+import { DepositModule } from "./deposit/deposit.module";
 
 @Module({
-  imports: [AuthModule, WalletModule],
+  imports: [AuthModule, WalletModule, DepositModule],
   controllers: [AppController],
   providers: [PrismaService],
   exports: [PrismaService],
