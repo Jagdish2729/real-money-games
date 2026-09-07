7573453175558f6b563b9d7f3e0e20db0ea870d2
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 20:31:13 +0530

    feat(api): register wallet module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { PrismaService } from "./prisma.service";
 import { AuthModule } from "./auth/auth.module";
+import { WalletModule } from "./wallet/wallet.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, WalletModule],
   controllers: [AppController],
   providers: [PrismaService],
   exports: [PrismaService],
