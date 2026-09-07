8dd8acd85b3209d94807bb7bb906c0976c686502
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 20:12:01 +0530

    Register authentication token service

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { TokenService } from "./token.service";
 import { PrismaService } from "../prisma.service";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, TokenService, PrismaService],
 })
 export class AuthModule {}
