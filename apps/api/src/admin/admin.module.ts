aed7b62e25af03113109f7b460af9cc4315d5cf4
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-07 22:17:53 +0530

    feat(api): register admin guard

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
 import { AdminController } from "./admin.controller";
+import { AdminGuard } from "./admin.guard";
 import { AdminService } from "./admin.service";
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService, PrismaService],
+  providers: [AdminService, AdminGuard, PrismaService],
 })
 export class AdminModule {}
