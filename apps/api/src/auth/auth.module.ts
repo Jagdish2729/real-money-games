2c05e174341c12231803dd7ba9e3dfc5cfae22de
Jagdish2729 <jagdishnegi2729@gmail.com>
2026-09-08 16:23:07 +0530

    Switch auth module from SMS OTP to email reset

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,12 +3,12 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "./auth.guard";
 import { TokenService } from "./token.service";
-import { OtpSmsService } from "./otp-sms.service";
+import { EmailService } from "./email.service";
 import { PrismaService } from "../prisma.service";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, AuthGuard, TokenService, OtpSmsService, PrismaService],
+  providers: [AuthService, AuthGuard, TokenService, EmailService, PrismaService],
   exports: [AuthGuard, TokenService],
 })
 export class AuthModule {}
